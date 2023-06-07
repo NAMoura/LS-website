ee20a2cf0b49d5be3ac62c6f9c1f7ff2c84d3a22
Nuno Moura <moura.nunoandre@gmail.com>
2023-06-07 18:48:47 +0100

    Gallery

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
-import { createClient } from "contentful"
+import { createClient } from "contentful";
+import ProjectCard from "../components/ProjectCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 
 export async function getStaticProps() {
 
@@ -18,10 +23,52 @@ export async function getStaticProps() {
 }
 
 export default function Projects( {projects} ) {
+  
+  const settings = {
+    dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow:1,
+            slidesToScroll: 1,
+            initialSlide: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
+
+
+
   console.log(projects);
   return (
     <div className="project-list">
-      Project List
+     <Slider {...settings}>
+      {projects.map(project => (
+        <ProjectCard key={project.sys.id} project={project}/> 
+      ))}
+      </Slider>
     </div>
   )
 }
